e73efde9f9279a7df367a833b2e140b72796d9d7
BrianVelarde02 <A01753036@tec.mx>
2025-03-22 15:24:42 -0600

    Agregando progreso al repositorio

---
+++ Assets/Scripts/MueveAccionPersonaje.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class MueveAccionPersonaje : MonoBehaviour
+{
+
+    // Para las 4 direcciones
+    [SerializeField]
+    private InputAction moveAction;
+
+    [SerializeField]
+    public const float SPEED = 10.0f;
+
+    public static Vector2 velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        transform.position = (Vector2)transform.position + move * SPEED * Time.deltaTime;
+
+        velocity = SPEED * move;
+    }
+
+}
